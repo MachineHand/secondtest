1a812bb51bdc3ed0105d7cc9a7c69687ea38023b
jae <kxmfkdlej@naver.com>
2021-01-27 16:04:11 +0900

    gugu

---
+++ .vscode/hello.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+
+int main(void)
+{
+int mul[3][9];
+
+for (int i = 0; i <3; i++)
+{
+	for (int j = 0; j < 9; j++)
+	{
+		mul[i][j] = (i + 2) * (j + 1);
+	}
+}
+
+for (int i = 0; i < 3; i++)
+{
+	for (int j = 0; j < 9; j++)
+	{
+		printf("%d  ", mul[i][j]);
+	}
+	printf("\n");
+}
+
+return 0;
+}
